c938b1363873727c89dbfd15d54f9c86b7c11b12
VincentG42 <goyetvincent@gmail.com>
2024-04-16 10:22:18 +0200

    test

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 
 function Configurator({edition}) {
 
+    const car = useSelector((state => state.config.version))
+
+    const [getId, setId] =useState()
+
+    function handle(event){
+        setId(event.target.id)
+    }
+
+    useEffect(() => {
+        console.log(getId);
+    }, [getId])
 
 
     return (
         <div>
-            Config , {edition}
+            {/* Config , {edition} */}
+            <button id = {car[0].name} onMouseOver={handle}> test moddafucka</button>
         </div>
     );
 }
--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,37 +1,49 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Configurator from '../Configurator/Configurator';
+import { useEffect } from 'react';
 
 function Selector() {
+    //useSelector pour avoir les infos des voitures venant de configuratorSlice
+    const cars = useSelector((store) => store.config.version);
+    
+
     //conditionnal rendering logic
-    const [selectorShown, setDiv1Shown] = useState(true);
+    const [selectorShown, setSelectorShown] = useState(true);
     const [chosenCarEdition, setChosenCarEdition] = useState();
-
-    const handleCardClick = (e) => {
-        setDiv1Shown(false);
-        setChosenCarEdition (e.target.id)
-
+    
+    function handleMouseOver(event) {
+        setChosenCarEdition(event.target.id);
+    }
+    
+    useEffect(() => {
         console.log(chosenCarEdition);
+        }, [chosenCarEdition]);
+    
+
+    const handleCardClick = () => {
+
+        setSelectorShown(false);
+
+        // console.log(chosenCarEdition);
     };
 
-    //useSelector pour avoir els infos des voitures venant de configuratorSlice
-    const cars = useSelector((store) => store.config.version);
 
 
     return (
         <div>
             {selectorShown ? (
-                <div id={cars[0].name}>
+                <div id={cars[0].name} className='flex flex-col justify-center items-center border-2 border-slate-500 p-5 w-2/5'>
                     <h3 className='text-xl font-bold'> Alpine A110 "{cars[0].name}" Edition</h3>
                     <p className='font-bold'><span className='text-slate-400'>Puissance</span> {cars[0].power}</p>
                     <p className='font-bold'><span className='text-slate-400'>Vitesse max.</span> {cars[0].maxSpeed} km/h</p>
                     <p className='font-bold'><span className='text-slate-400'>Boite de vitesse</span> {cars[0].transmission}</p>
-                    <button id = {cars[0].name} onClick={handleCardClick} className ='relative px-5 py-2.5 overflow-hidden group bg-slate-500 relative hover:bg-gradient-to-r hover:from-slate-500 hover:to-slate-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-slate-400 transition-all ease-out duration-300'> 
-                    <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span><span className="relative">Choisir</span></button>
+                    <button id={cars[0].name} onMouseEnter={handleMouseOver} onClick={handleCardClick} className='relative px-5 py-2.5 overflow-hidden group bg-slate-500 relative hover:bg-gradient-to-r hover:from-slate-500 hover:to-slate-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-slate-400 transition-all ease-out duration-300'>
+                        <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span><span className="relative">Choisir</span></button>
 
                 </div>
             ) : (
-                <Configurator edition = {chosenCarEdition} />
+                <Configurator edition={chosenCarEdition} />
             )}
         </div>
     );
