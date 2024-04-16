31715a829ebf3460984f0ef8aa0c3eec322e9061
VincentG42 <goyetvincent@gmail.com>
2024-04-16 10:56:59 +0200

    test2

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -6,20 +6,20 @@ import { useEffect } from 'react';
 function Selector() {
     //useSelector pour avoir les infos des voitures venant de configuratorSlice
     const cars = useSelector((store) => store.config.version);
-    
+
 
     //conditionnal rendering logic
     const [selectorShown, setSelectorShown] = useState(true);
     const [chosenCarEdition, setChosenCarEdition] = useState();
-    
+
     function handleMouseOver(event) {
         setChosenCarEdition(event.target.id);
     }
-    
+
     useEffect(() => {
         console.log(chosenCarEdition);
-        }, [chosenCarEdition]);
-    
+    }, [chosenCarEdition]);
+
 
     const handleCardClick = () => {
 
@@ -33,14 +33,17 @@ function Selector() {
     return (
         <div>
             {selectorShown ? (
-                <div id={cars[0].name} className='flex flex-col justify-center items-center border-2 border-slate-500 p-5 w-2/5'>
-                    <h3 className='text-xl font-bold'> Alpine A110 "{cars[0].name}" Edition</h3>
-                    <p className='font-bold'><span className='text-slate-400'>Puissance</span> {cars[0].power}</p>
-                    <p className='font-bold'><span className='text-slate-400'>Vitesse max.</span> {cars[0].maxSpeed} km/h</p>
-                    <p className='font-bold'><span className='text-slate-400'>Boite de vitesse</span> {cars[0].transmission}</p>
-                    <button id={cars[0].name} onMouseEnter={handleMouseOver} onClick={handleCardClick} className='relative px-5 py-2.5 overflow-hidden group bg-slate-500 relative hover:bg-gradient-to-r hover:from-slate-500 hover:to-slate-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-slate-400 transition-all ease-out duration-300'>
-                        <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span><span className="relative">Choisir</span></button>
-
+                <div className='flex  justify-center items-center'>
+                    {cars.map(car => (
+                        <div id={car.name} className='flex flex-col justify-center items-center border-y-2 border-x border-slate-900 p-5 w-1/2 gap-4'>
+                            <h3 className='text-xl font-bold'> Alpine A110 "{car.name}" Edition</h3>
+                            <p className='font-bold'><span className='text-slate-400'>Puissance :</span> {car.power}</p>
+                            <p className='font-bold'><span className='text-slate-400'>Vitesse max. :</span> {car.maxSpeed} km/h</p>
+                            <p className='font-bold'><span className='text-slate-400'>Boite de vitesse :</span> {car.transmission}</p>
+                            <button id={car.name} onMouseEnter={handleMouseOver} onClick={handleCardClick} className='relative px-5 py-2.5 overflow-hidden group bg-slate-900 relative hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-600 text-white hover:ring-2 hover:ring-offset-2 hover:ring-slate-900 transition-all ease-out duration-300'>
+                                <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span><span className="relative">Choisir</span></button>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <Configurator edition={chosenCarEdition} />
